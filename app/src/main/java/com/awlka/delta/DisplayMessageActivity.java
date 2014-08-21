1488b349f65fd2162f5a72ddc54dfe03f7bd58c6
Lucas Churchill <lucas@agtlucas.com>
2014-08-21 14:22:50 -0300

    Remove onCreateOptionsMenu

--- app/src/main/java/com/awlka/delta/DisplayMessageActivity.java
+++ app/src/main/java/com/awlka/delta/DisplayMessageActivity.java
@@ -16,13 +16,6 @@ public class DisplayMessageActivity extends Activity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.display_message, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
