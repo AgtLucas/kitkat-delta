d6e4f2542248885bb26fbd14cf95083f0ec576ec
Lucas Churchill <lucas@agtlucas.com>
2014-08-21 14:17:33 -0300

    Start activity

--- app/src/main/java/com/awlka/delta/MyActivity.java
+++ app/src/main/java/com/awlka/delta/MyActivity.java
@@ -44,5 +44,6 @@ public class MyActivity extends Activity {
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
