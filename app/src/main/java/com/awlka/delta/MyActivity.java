8e57df291f74ef2d1da3a054a12d8f6b35b8c37e
Lucas Churchill <lucas@agtlucas.com>
2014-08-21 14:16:37 -0300

    Update MyActivity.java

--- app/src/main/java/com/awlka/delta/MyActivity.java
+++ app/src/main/java/com/awlka/delta/MyActivity.java
@@ -4,9 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 
 public class MyActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "com.awlka.delta.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +37,12 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
     }
 }
